ca91cd4f2c91d0c97d787489946efd5828f68707
Hung_Nguyen <hung@agencyrevolution.com>
2015-04-01 17:25:18 +0700

    @Add - Service class to communicate with micro-service. Simple load-balancing supported.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.KafkaWorker = require('./lib/kafkaWorker');
 exports.KafkaWorkerPool = require('./lib/kafkaWorkerPool');
 exports.systemTime = require('./lib/systemTime');
+exports.Service = require('./lib/service');
