2c5725afb7d3b15423dd5af35ec227b02e07351f
Hung_Nguyen <hung@agencyrevolution.com>
2015-04-13 19:09:57 +0700

    @Modify - `Service.send()` will reject if all endpoints are down
    @Add
    - `RuleEngine` class which expose one function `findMatches(rule, array)` to return list of items in given array which match any/all conditions in rule (not test & publish yet)

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+'use strict';
+
+
+/**
+ * KafkaWorker class
+ * @type {class}
+ */
 exports.KafkaWorker = require('./lib/kafkaWorker');
+
+
+/**
+ * KafkaWorkerPool class
+ * @type {class}
+ */
 exports.KafkaWorkerPool = require('./lib/kafkaWorkerPool');
-exports.systemTime = require('./lib/systemTime');
+
+
+/**
+ * Service class
+ * @type {class}
+ */
 exports.Service = require('./lib/service');
+
+
+/**
+ * RuleEngine class
+ * @type {class}
+ */
+exports.RuleEngine = require('./lib/rule-engine/');
+
+
+/**
+ * Sytem time
+ * @type {object}
+ */
+exports.systemTime = require('./lib/systemTime');
